d7249570e9963e26d6964583beb29d4224e5b354
chandan-akshronix <chandan.taksande@akshronix.com>
2025-05-28 15:01:14 +0000

    Modified inference processing 6

--- pipelines/Fraud/pipeline.py
+++ pipelines/Fraud/pipeline.py
@@ -575,81 +575,81 @@ def get_pipeline(
         depends_on=["FraudTransform"]
     )
 
-    model_metrics = ModelMetrics(
-        model_data_statistics=MetricsSource(
-            s3_uri=data_quality_check_step.properties.CalculatedBaselineStatistics,
-            content_type="application/json",
-        ),
-        model_data_constraints=MetricsSource(
-            s3_uri=data_quality_check_step.properties.CalculatedBaselineConstraints,
-            content_type="application/json",
-        ),
-        bias_pre_training=MetricsSource(
-            s3_uri=data_bias_check_step.properties.CalculatedBaselineConstraints,
-            content_type="application/json",
-        ),
-        model_statistics=MetricsSource(
-            s3_uri=model_quality_check_step.properties.CalculatedBaselineStatistics,
-            content_type="application/json",
-        ),
-        model_constraints=MetricsSource(
-            s3_uri=model_quality_check_step.properties.CalculatedBaselineConstraints,
-            content_type="application/json",
-        ),
-        bias_post_training=MetricsSource(
-            s3_uri=model_bias_check_step.properties.CalculatedBaselineConstraints,
-            content_type="application/json",
-        ),
-        bias=MetricsSource(
-            # This field can also be set as the merged bias report
-            # with both pre-training and post-training bias metrics
-            s3_uri=model_bias_check_step.properties.CalculatedBaselineConstraints,
-            content_type="application/json",
-        ),
-        explainability=MetricsSource(
-            s3_uri=model_explainability_check_step.properties.CalculatedBaselineConstraints,
-            content_type="application/json",
-        )
-    )
+    # model_metrics = ModelMetrics(
+    #     model_data_statistics=MetricsSource(
+    #         s3_uri=data_quality_check_step.properties.CalculatedBaselineStatistics,
+    #         content_type="application/json",
+    #     ),
+    #     model_data_constraints=MetricsSource(
+    #         s3_uri=data_quality_check_step.properties.CalculatedBaselineConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     bias_pre_training=MetricsSource(
+    #         s3_uri=data_bias_check_step.properties.CalculatedBaselineConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     model_statistics=MetricsSource(
+    #         s3_uri=model_quality_check_step.properties.CalculatedBaselineStatistics,
+    #         content_type="application/json",
+    #     ),
+    #     model_constraints=MetricsSource(
+    #         s3_uri=model_quality_check_step.properties.CalculatedBaselineConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     bias_post_training=MetricsSource(
+    #         s3_uri=model_bias_check_step.properties.CalculatedBaselineConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     bias=MetricsSource(
+    #         # This field can also be set as the merged bias report
+    #         # with both pre-training and post-training bias metrics
+    #         s3_uri=model_bias_check_step.properties.CalculatedBaselineConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     explainability=MetricsSource(
+    #         s3_uri=model_explainability_check_step.properties.CalculatedBaselineConstraints,
+    #         content_type="application/json",
+    #     )
+    # )
 
-    drift_check_baselines = DriftCheckBaselines(
-        model_data_statistics=MetricsSource(
-            s3_uri=data_quality_check_step.properties.BaselineUsedForDriftCheckStatistics,
-            content_type="application/json",
-        ),
-        model_data_constraints=MetricsSource(
-            s3_uri=data_quality_check_step.properties.BaselineUsedForDriftCheckConstraints,
-            content_type="application/json",
-        ),
-        bias_pre_training_constraints=MetricsSource(
-            s3_uri=data_bias_check_step.properties.BaselineUsedForDriftCheckConstraints,
-            content_type="application/json",
-        ),
-        bias_config_file=FileSource(
-            s3_uri=model_bias_check_config.monitoring_analysis_config_uri,
-            content_type="application/json",
-        ),
-        model_statistics=MetricsSource(
-            s3_uri=model_quality_check_step.properties.BaselineUsedForDriftCheckStatistics,
-            content_type="application/json",
-        ),
-        model_constraints=MetricsSource(
-            s3_uri=model_quality_check_step.properties.BaselineUsedForDriftCheckConstraints,
-            content_type="application/json",
-        ),
-        bias_post_training_constraints=MetricsSource(
-            s3_uri=model_bias_check_step.properties.BaselineUsedForDriftCheckConstraints,
-            content_type="application/json",
-        ),
-        explainability_constraints=MetricsSource(
-            s3_uri=model_explainability_check_step.properties.BaselineUsedForDriftCheckConstraints,
-            content_type="application/json",
-        ),
-        explainability_config_file=FileSource(
-            s3_uri=model_explainability_check_config.monitoring_analysis_config_uri,
-            content_type="application/json",
-        )
-    )
+    # drift_check_baselines = DriftCheckBaselines(
+    #     model_data_statistics=MetricsSource(
+    #         s3_uri=data_quality_check_step.properties.BaselineUsedForDriftCheckStatistics,
+    #         content_type="application/json",
+    #     ),
+    #     model_data_constraints=MetricsSource(
+    #         s3_uri=data_quality_check_step.properties.BaselineUsedForDriftCheckConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     bias_pre_training_constraints=MetricsSource(
+    #         s3_uri=data_bias_check_step.properties.BaselineUsedForDriftCheckConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     bias_config_file=FileSource(
+    #         s3_uri=model_bias_check_config.monitoring_analysis_config_uri,
+    #         content_type="application/json",
+    #     ),
+    #     model_statistics=MetricsSource(
+    #         s3_uri=model_quality_check_step.properties.BaselineUsedForDriftCheckStatistics,
+    #         content_type="application/json",
+    #     ),
+    #     model_constraints=MetricsSource(
+    #         s3_uri=model_quality_check_step.properties.BaselineUsedForDriftCheckConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     bias_post_training_constraints=MetricsSource(
+    #         s3_uri=model_bias_check_step.properties.BaselineUsedForDriftCheckConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     explainability_constraints=MetricsSource(
+    #         s3_uri=model_explainability_check_step.properties.BaselineUsedForDriftCheckConstraints,
+    #         content_type="application/json",
+    #     ),
+    #     explainability_config_file=FileSource(
+    #         s3_uri=model_explainability_check_config.monitoring_analysis_config_uri,
+    #         content_type="application/json",
+    #     )
+    # )
 
     ### Register the model
 
@@ -678,8 +678,8 @@ def get_pipeline(
         transform_instances=["ml.m5.large"],
         model_package_group_name=model_package_group_name,
         approval_status=model_approval_status,
-        model_metrics=model_metrics,
-        drift_check_baselines=drift_check_baselines,
+        # model_metrics=model_metrics,
+        # drift_check_baselines=drift_check_baselines,
         customer_metadata_properties={"preprocess_s3_path": preprocess_s3_path}
     )
     step_register = ModelStep(
